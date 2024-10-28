8d782164b8119ea5286784d9f60547f70ee37884
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-28 16:29:39 +0000

    chore(password input): appease linter

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { InputProps, Group } from 'react-aria-components';
-import { Input } from './field';
-import { ToggleButton } from './button';
-import { composeTailwindRenderProps } from './utils';
-import { EyeIcon, EyeOffIcon } from './icons';
+import React from "react";
+import { Group, type InputProps } from "react-aria-components";
 
+import { ToggleButton } from "./button";
+import { Input } from "./field";
+import { EyeIcon, EyeOffIcon } from "./icons";
+import { composeTailwindRenderProps } from "./utils";
+
+export default {};
 export function PasswordInput(props: InputProps) {
+  const { className } = props;
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
 
   return (
     <Group
       data-ui="control"
       className={[
-        'grid',
-        'grid-cols-[1fr_calc(theme(size.5)+20px)]',
-        'sm:grid-cols-[1fr_calc(theme(size.4)+20px)]',
-      ].join(' ')}
+        "grid",
+        "grid-cols-[1fr_calc(theme(size.5)+20px)]",
+        "sm:grid-cols-[1fr_calc(theme(size.4)+20px)]",
+      ].join(" ")}
     >
       <Input
         {...props}
-        className={composeTailwindRenderProps(props.className, [
-          'peer',
-          'col-span-full',
-          'row-start-1',
-          'pe-10 sm:pe-9',
+        className={composeTailwindRenderProps(className, [
+          "peer",
+          "col-span-full",
+          "row-start-1",
+          "pe-10 sm:pe-9",
         ])}
-        type={isPasswordVisible ? 'text' : 'password'}
+        type={isPasswordVisible ? "text" : "password"}
       />
       <ToggleButton
         isIconOnly
@@ -35,12 +38,12 @@ export function PasswordInput(props: InputProps) {
         isSelected={isPasswordVisible}
         onChange={setIsPasswordVisible}
         className={[
-          'group',
-          'focus-visible:-outline-offset-1',
-          'row-start-1',
-          '-col-end-1',
-          'place-self-center',
-        ].join(' ')}
+          "group",
+          "focus-visible:-outline-offset-1",
+          "row-start-1",
+          "-col-end-1",
+          "place-self-center",
+        ].join(" ")}
       >
         {isPasswordVisible ? (
           <EyeOffIcon className="text-muted/75 group-hover:text-foreground" />
